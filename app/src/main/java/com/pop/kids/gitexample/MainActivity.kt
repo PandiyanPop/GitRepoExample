c5422ffb896ecd11969c411a4278d5da10b5d41e
karthickpandiyan <pandiyan.cse2012@gmail.com>
2022-12-28 21:13:59 +0530

    Test commit

---
+++ app/src/main/java/com/pop/kids/gitexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pop.kids.gitexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
